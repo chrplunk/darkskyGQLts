560d5f167d8bab2b14afd5fd7961239aa8f4380d
chrplunk <30636645+chrplunk@users.noreply.github.com>
2018-12-27 17:43:16 -0500

    mappedResult
    Signed-off-by: chrplunk <30636645+chrplunk@users.noreply.github.com>

--- src/modules/darksky/resolvers.ts
+++ src/modules/darksky/resolvers.ts
@@ -8,20 +8,32 @@ export const resolvers: ResolverMap = {
         
         weatherQuery: async (_: any, args: any, context: any) => {
             
-            const darkskyurl = "https://api.darksky.net/forecast/"
-            const darkskykey = "2f2275e2eb17536af6b1918e44dd63bf/" // put in .env later
-            let latlong = "37.8267,-122.4233" //change later to args.latlong or something
+            try {
+                const darkskyurl = "https://api.darksky.net/forecast/"
+                const darkskykey = "2f2275e2eb17536af6b1918e44dd63bf/" // put in .env later
+                let latlong = "37.8267,-122.4233" //change later to args.latlong or something
 
-            var options = { "url": darkskyurl + darkskykey + latlong }    
-            
-            console.log("The url to DarkSky is:" + options.url);
-            const response = await fetch(options.url);
-            const result = await response.json();
-            
-            console.log("JSON result from DarkSky is:" + result);
-            return result;
-            
-            //return fakeweather[0]
+                var options = { "url": darkskyurl + darkskykey + latlong }    
+                
+                console.log("******* The url to DarkSky is:" + options.url);
+                const response = await fetch(options.url);
+                const result = await response.json();
+                            
+                let mappedResult = {
+                    timezone: result.timezone,
+                    currentlyTime: result.currently.timezone,
+                    currentlySummary: result.currently.summary,
+                    currentlyHumidity: result.currently.humidity
+                }
+                
+                console.log("timezone: " + mappedResult.timezone);
+                console.log("current time: " + mappedResult.currentlyTime);
+                console.log("current summary: " + mappedResult.currentlySummary);
+                console.log("current humidity: " + mappedResult.currentlyHumidity);
+
+                return mappedResult;
+            } 
+            catch (e) { console.log(e); }
         }
     }
 }
