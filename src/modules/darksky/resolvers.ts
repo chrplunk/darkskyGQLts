cb3550729dbfc613c5023a2f735b4073e11064f8
chrplunk <30636645+chrplunk@users.noreply.github.com>
2018-12-27 17:02:47 -0500

    added fetch phase 1
    Signed-off-by: chrplunk <30636645+chrplunk@users.noreply.github.com>

--- src/modules/darksky/resolvers.ts
+++ src/modules/darksky/resolvers.ts
@@ -1,11 +1,27 @@
 import { ResolverMap } from "../../types/graphqltypes";
-import { fakeweather } from "./fakeweather";
+//import { fakeweather } from "./fakeweather";
+
+const fetch = require("node-fetch");
 
 export const resolvers: ResolverMap = {
     Query: {
         
         weatherQuery: async (_: any, args: any, context: any) => {
-            return fakeweather[0]
+            
+            const darkskyurl = "https://api.darksky.net/forecast/"
+            const darkskykey = "2f2275e2eb17536af6b1918e44dd63bf/"
+            let latlong = "37.8267,-122.4233" //change later to args.latlong or something
+
+            var options = { "url": darkskyurl + darkskykey + latlong }    
+            
+            console.log("The url to DarkSky is:" + options.url);
+            const response = await fetch(options.url);
+            const result = await response.json();
+            
+            console.log("JSON result from DarkSky is:" + result);
+            return result;
+            
+            //return fakeweather[0]
         }
     }
 }
